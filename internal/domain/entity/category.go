3a2a50429c25e2349f5b2bb6a46f923d0ab0ca00
reinhardjs <reinhardjsilalahi@gmail.com>
2025-01-07 22:21:10 +0700

    update category and product entity

--- internal/domain/entity/category.go
+++ internal/domain/entity/category.go
@@ -1,6 +1,15 @@
 package entity
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Category struct {
-	ID   uint   `gorm:"primaryKey"`
-	Name string `gorm:"size:100"`
+	ID        uint           `gorm:"primaryKey"`
+	Name      string         `gorm:"size:100;not null"`
+	CreatedAt time.Time      `gorm:"autoCreateTime"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
--- internal/domain/entity/product.go
+++ internal/domain/entity/product.go
@@ -1,9 +1,18 @@
 package entity
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Product struct {
-	ID         uint   `gorm:"primaryKey"`
-	Name       string `gorm:"size:100"`
-	Price      float64
-	CategoryID uint
-	Category   Category
+	ID         uint           `gorm:"primaryKey"`
+	Name       string         `gorm:"size:100;not null"`
+	Price      float64        `gorm:"not null"`
+	CategoryID uint           `gorm:"not null"`
+	Category   Category       `gorm:"foreignKey:CategoryID"`
+	CreatedAt  time.Time      `gorm:"autoCreateTime"`
+	UpdatedAt  time.Time      `gorm:"autoUpdateTime"`
+	DeletedAt  gorm.DeletedAt `gorm:"index"`
 }
